b6ff306e84ae55308cbb817cb8716c95e0d53392
IsPekToP <inspektop1@gmail.com>
2024-07-19 09:55:54 +0200

    убрал из setting.py емейл

--- 17main3_1/itProger/itProger/settings.py
+++ 17main3_1/itProger/itProger/settings.py
@@ -122,8 +122,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'inspektop1@gmail.com'
-# EMAIL_HOST_PASSWORD = 'trux qchy acpn iphn'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
