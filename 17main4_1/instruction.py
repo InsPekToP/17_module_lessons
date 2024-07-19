7e263d9d7705e60cc5b587dfadceb553eb4349a9
IsPekToP <inspektop1@gmail.com>
2024-07-19 16:02:01 +0200

    описание выгрузки на сервер

--- 17main4_1/instruction.py
+++ 17main4_1/instruction.py
@@ -1,3 +1,15 @@
 #17main1_1
 #Публикация сайта на сервер
 
+#проверка на наличие Heroku -- heroku прописать в терминале 
+#для логина -- heroku login
+#дальше любую клавишу или q - для выхода.
+#pip install gunicorn
+#создаем requirements.txt
+#pip freeze
+#pip freeze >requirements.txt
+#надо перекинуть в папку с проектом
+#дальше git init и добавить gitignore
+#https://github.com/github/gitignore/blob/main/Python.gitignore
+
+#если работаем на Mac добавляем .DC_Store
